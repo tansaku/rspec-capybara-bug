deed80e2f1bb8074cb145843a0602ae8c92e1288
Sam Joseph <tansaku@gmail.com>
2014-09-30 10:04:36 +0100

    adjusting spec with new info

--- spec/sinatra_app_spec.rb
+++ spec/sinatra_app_spec.rb
@@ -2,15 +2,10 @@ require 'spec_helper'
 
 describe SinatraApp do
 
-  it 'unfortunately conceals sinatra errors and does not break tests where it should' do
+  it 'unfortunately conceals sinatra errors not detected due to source code pulled into error page' do
     visit '/sinatra_error'
     save_and_open_page
     expect(page).to have_content '2iuwqo7ykhg23uikqwdjg2kh3ihqwdqkwj'
-  end
-
-  it 'but works as long as there are no sinatra errors' do
-    visit '/sinatra_error'
-    expect(page).to have_content 'A string that does not appear in the sinatra error page'
   end
 
   it 'and fails correctly as long as there are no sinatra errors' do
